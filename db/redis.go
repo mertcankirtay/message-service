052195896e469aeb69cd48b3f0ba32d23deb73c6
mertcankirtay <mertcan.kirtay@icloud.com>
2025-07-09 16:12:01 +0300

    fix: typo in redis.go

--- db/redis.go
+++ db/redis.go
@@ -7,22 +7,22 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-var redisClient *redis.Client
+var RedisClient *redis.Client
 
 func InitRedis() (err error) {
 	// Start redis connection
-	redisClient = redis.NewClient(&redis.Options{
+	RedisClient = redis.NewClient(&redis.Options{
 		Addr:     "redis:6379",
 		Password: os.Getenv("REDIS_PASSWORD"),
 		DB:       0,
 	})
 
 	// Check if connection is successful
-	_, err = redisClient.Ping(context.TODO()).Result()
+	_, err = RedisClient.Ping(context.TODO()).Result()
 
 	return
 }
 
 func DisconnectRedis() {
-	redisClient.Close()
+	RedisClient.Close()
 }
